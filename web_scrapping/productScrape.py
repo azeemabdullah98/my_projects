800de67dc348c9a09833f3a9ead9cde0285b788a
azeemdrake <azeemquest98@gmail.com>
2021-07-06 21:27:54 +0530

    changes made on 06/07/2021

--- web_scrapping/productScrape.py
+++ web_scrapping/productScrape.py
@@ -2,13 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
-from csv import DictReader, DictWriter
-import csv
 
 
 def ProductScrape(url):
     html=requests.get(url).text
-    soup = BeautifulSoup(url,'lxml')
+    soup = BeautifulSoup(html,'lxml')
     for name in soup.find_all("h3",class_="product-name"):
         title=name.text
     price=soup("app-price-display")
@@ -25,12 +23,9 @@ def ProductScrape(url):
     for weight in soup.find_all("span",class_="callout"):
         if weight.text.startswith("Gross"):
             gross_weight.append(weight.text)
-        elif weight.text.startswith("Net"):
+        if weight.text.startswith("Net"):
             net_weight.append(weight.text)
-        else:
+            return gross_weight,net_weight,amount
+        elif weight.text.startswith("Weight"):
             total_weight.append(weight.text)
-    field_names=['Name','Product Code','Product Name','Unit of Measurement']
-    with open("/mnt/c/python_projects/fipola/cc_final.csv",'a+') as df:
-        if title in df['Product Name']:
-            dictWriter = DictWriter(df,)
-
+            return total_weight,amount
