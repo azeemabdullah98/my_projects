de2dbab9437b82611884d8fe2a0cdfd206b07f68
Azeem Abdullah K <79804540+azeemabdullah98@users.noreply.github.com>
2021-12-01 15:47:48 +0530

    Delete python_projects/junks directory

--- python_projects/junks/Tax.py
+++
@@ -1,8 +0,0 @@
-# to check if tax is available for the particular discom else return none...
-def CheckTax(new_df):
-    try:
-        tax = new_df[(new_df['Line Text'] == 'Tax')]
-        tax = new_df.at[tax.index[0],'Cond Value']
-        return tax
-    except Exception as e:
-        return None
