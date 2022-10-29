c588f89bc0a5ee3c703f87462eb2c9b43af37689
Azeem Abdullah <azeemquest98@gmail.com>
2022-10-29 15:56:42 +0530

    minor bug fixes

--- python_projects/flask_blog/run.py
+++ python_projects/flask_blog/run.py
@@ -5,10 +5,3 @@ from flask_blog import app
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# import os
-# username = os.environ.get('EMAIL_USER')
-# password = os.environ.get('EMAIL_PASS')
-
-# print(username)
-# print(password)
